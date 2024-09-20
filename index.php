7957a7edc475cc31052478e1dc3da55403a54532
M2Y2 <31753974+ThanHtunAung-THA@users.noreply.github.com>
2024-09-20 16:35:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="refresh" content="0;url=public/">
-  <title>Overseas Jobs Agency</title>
+  <title>Overseas Jobs Central</title>
 </head>
 <body>
   <p>If you are not redirected automatically, <a href="public/">click here to go to the main page</a>.</p>
